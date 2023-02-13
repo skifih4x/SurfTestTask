e73949626e7fda3485e724be082929302b97c628
Artem <artem-orlov92@bk.ru>
2023-02-13 17:26:48 +0300

    fix: Убрал storyboard и добавил главную картинку

--- SurfTestTask/ViewController.swift
+++ SurfTestTask/ViewController.swift
@@ -7,13 +7,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+    
+    let mainImage: UIImageView = {
+        let image = UIImageView()
+        image.image = UIImage(named: "MainImage")
+        image.translatesAutoresizingMaskIntoConstraints = false
+        image.contentMode = .scaleToFill
+        return image
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.addSubview(mainImage)
+        view.backgroundColor = .red
+        NSLayoutConstraint.activate([
+            mainImage.topAnchor.constraint(equalTo: view.topAnchor),
+            mainImage.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            mainImage.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            mainImage.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
     }
-
-
 }
 
