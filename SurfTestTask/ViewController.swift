533287413ab69b6c0ab20d2279f5a8caffa93fde
Artem <artem-orlov92@bk.ru>
2023-02-13 14:16:33 +0300

    Initial Commit

---
+++ SurfTestTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SurfTestTask
+//
+//  Created by Артем Орлов on 13.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
